8623a8ab4637a54b0c7119a4283d5afa23f7f4e0
kimgorok <kjyse2@naver.com>
2024-07-20 00:09:20 +0900

    ✨ [견적서페이지]: 상품 정보 컴포넌트화 이전

--- src/app/(route)/quotation/page.tsx
+++ src/app/(route)/quotation/page.tsx
@@ -6,23 +6,27 @@ import Input from '@/app/components/common/Input';
 export default function Quotation() {
   const [inputState, setInputState] = useState({
     search: '',
+    count: '',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: any,
+  ) => {
     const { value } = e.target;
     setInputState((prev) => ({
       ...prev,
-      search: value,
+      [type]: value,
     }));
   };
 
   return (
     <section>
-      <div className="bg-white w-full h-screen flex flex-col">
+      <div className="bg-white w-full h-screen flex relative">
         {/* 네비게이션 바 */}
-        <div className="absolute bg-primary-3 sm:w-26 lg:w-[160px] h-screen">
+        <div className="bg-primary-3 w-32">
           <p className="pt-6 h-4 text-white font-black">정말맛있는푸드</p>
-          <div className="mt-12 flex-center flex-col">
+          <div className="mt-12 flex-center flex-col cursor-pointer">
             <p className="flex-center h-9 w-full font-black text-white text-lg hover:bg-primary-4 border-b-[1px] border-gray-1">
               주문생성
             </p>
@@ -41,28 +45,28 @@ export default function Quotation() {
         </div>
 
         {/* 메인 컨테이너 */}
-        <div className="sm:ml-28 lg:ml-[160px] mt-14 px-4 py-2">
+        <div className="mt-14 px-4 py-2 w-full">
           <div className="flex gap-4">
             <button
-              className="bg-primary-4 text-lg font-black text-white px-2 py-1"
+              className="bg-primary-4 text-lg font-black text-white px-2 whitespace-nowrap"
               type="button"
               onClick={() => {}}
             >
               최근주문목록
             </button>
             <Input
-              className="bg-gray-0 md:w-64 lg:w-[580px] px-2 py-3 text-xl placeholder:text-xl placeholder:font-black"
+              className="bg-gray-0 md:w-96 px-2 py-1 text-xl font-black placeholder:text-xl placeholder:font-black"
               value={inputState.search}
               type="text"
-              onChange={handleInputChange}
+              onChange={(e) => handleInputChange(e, 'search')}
               placeholder="검색어를 입력해주세요"
             />
           </div>
 
-          <div className="w-full bg-primary-4 mt-4">
+          <div className="md:w-full bg-primary-4 mt-4 w-[540px]">
             {/* 분류 품번 품명 단위 개수 */}
-            <div className="flex justify-between text-white text-2xl font-black px-4 py-1">
-              <div className="flex gap-14">
+            <div className="flex justify-between text-white text-xl font-black pl-4 pr-5 py-1">
+              <div className="flex gap-8">
                 <p>분류</p>
                 <p>품번</p>
               </div>
@@ -70,19 +74,44 @@ export default function Quotation() {
               <p>품명</p>
 
               <div className="flex gap-8">
-                <p>단위</p>
                 <p>개수</p>
+                <p>단위</p>
                 <p>담기</p>
               </div>
             </div>
 
             {/* 목록창 */}
-            <div className="bg-gray-0 px-4 h-80">하잉</div>
+            <div className="bg-white px-3 h-80 flex-col border-2 whitespace-nowrap">
+              {/* 개별 목록 */}
+              <div className="flex justify-between py-2 w-full text-gray-9 text-xl font-bold border-b-2">
+                <div className="flex gap-4">
+                  <p>냉동</p>
+                  <p>SAM-572</p>
+                </div>
+                <p>하와이안피자 하와이안피자 하와이안피자</p>
+                <div className="flex gap-10">
+                  <Input
+                    className="w-10 text-right"
+                    placeholder="0"
+                    value={inputState.count}
+                    type="text"
+                    onChange={(e) => handleInputChange(e, 'count')}
+                  />
+                  <p>kg</p>
+                  <button
+                    type="button"
+                    className="bg-primary-4 text-white px-1"
+                  >
+                    담기
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="w-full bg-primary-4 mt-4">
+          <div className="md:w-full bg-primary-4 mt-4 w-[540px]">
             {/* 분류 품번 품명 단위 개수 */}
-            <div className="flex text-white text-2xl font-black px-4 py-1">
+            <div className="flex text-white text-xl font-black px-4 py-1">
               추가한 상품
             </div>
 
