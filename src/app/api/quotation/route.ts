5f3b78b4241caa841a9222ef8824ecd598a7c04e
kimgorok <kjyse2@naver.com>
2024-07-23 22:22:05 +0900

    ✨[견적서페이지]: 검색 interface 생성

--- src/app/api/quotation/route.ts
+++ src/app/api/quotation/route.ts
@@ -4,10 +4,10 @@ import { getSearchProducts } from '@/app/service/getRequest';
 export async function GET(request: Request) {
   try {
     const url = new URL(request.url);
-    const namePrefix = url.searchParams.get('name_prefix');
+    const namePrefix = url.searchParams.get('name_prefix') || '';
     const limit = url.searchParams.get('limit') || '10'; // default value: 10
     const cachedTime = url.searchParams.get('cached_time') || '300'; // default value: 300
-    const token = request.headers.get('access-token');
+    const token = request.headers.get('access-token') || '';
 
     console.log(`Request URL: ${url}`);
     console.log(`namePrefix: ${namePrefix}`);
