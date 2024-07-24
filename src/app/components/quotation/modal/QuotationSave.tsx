5ea2bb2408aacdfcd4ce397b4aa7cdc89ca4f97e
Minkyu0424 <yng1404@gmail.com>
2024-07-25 02:10:41 +0900

    :feat:[견적서모달]: 견적서 모달 UI 생성

---
+++ src/app/components/quotation/modal/QuotationSave.tsx
@@ -0,0 +1,13 @@
+import { MODAL_TEXT } from '@/app/constants/quotation';
+import CameraIcon from '@/app/ui/Icons/Camera';
+
+const QuotationSave = () => {
+  return (
+    <div className="flex-center w-[152px] h-10 border-2 rounded-3xl border-black gap-x-1 cursor-pointer">
+      <CameraIcon />
+      <div className='text-lg'>{MODAL_TEXT[4]}</div>
+    </div>
+  );
+};
+
+export default QuotationSave;
