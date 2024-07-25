e267c1dc5847207fb04f7ac7a5ca478a2a30b4c4
Minkyu0424 <yng1404@gmail.com>
2024-07-25 23:52:00 +0900

    :recycle:[견적서모달]: 아이콘 함수형 컴포넌트 선언으로 변경

--- src/app/ui/Icons/Camera.tsx
+++ src/app/ui/Icons/Camera.tsx
@@ -1,4 +1,4 @@
-const CameraIcon = () => {
+export default function CameraIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -23,6 +23,4 @@ const CameraIcon = () => {
       />
     </svg>
   );
-};
-
-export default CameraIcon;
+}
