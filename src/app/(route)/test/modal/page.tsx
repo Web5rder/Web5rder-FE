10564622440a04d64e8b88d28f3020182f0f7514
Minkyu0424 <yng1404@gmail.com>
2024-07-25 23:55:01 +0900

    :recycle:[견적서모달]: 타입 파일 에러 수정

--- src/app/(route)/test/modal/page.tsx
+++ src/app/(route)/test/modal/page.tsx
@@ -2,7 +2,7 @@ import QuotationModal from '@/app/components/quotation/modal/QuotationModal';
 
 const QuotationPage = () => {
   return (
-    <div className='w-full h-screen flex-center bg-primary-4'>
+    <div className="w-full h-screen flex-center bg-primary-4">
       <QuotationModal />
     </div>
   );
