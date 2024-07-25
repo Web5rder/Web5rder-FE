db06477e25482862a87590a5782fa13a025d0634
kimgorok <kjyse2@naver.com>
2024-07-25 17:38:32 +0900

    ✨[견적서페이지] : route.ts Promise 추가

--- src/app/api/quotation/route.ts
+++ src/app/api/quotation/route.ts
@@ -1,19 +1,13 @@
 import { NextResponse } from 'next/server';
 import { getSearchProducts } from '@/app/service/getRequest';
 
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<NextResponse> {
   try {
     const url = new URL(request.url);
     const namePrefix = url.searchParams.get('name_prefix') || '';
     const limit = url.searchParams.get('limit') || '10'; // default value: 10
     const cachedTime = url.searchParams.get('cached_time') || '300'; // default value: 300
     const token = request.headers.get('access-token') || '';
-
-    console.log(`Request URL: ${url}`);
-    console.log(`namePrefix: ${namePrefix}`);
-    console.log(`limit: ${limit}`);
-    console.log(`cachedTime: ${cachedTime}`);
-    console.log(`Token: ${token}`);
 
     const data = await getSearchProducts({
       namePrefix,
