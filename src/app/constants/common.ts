f70c9c5f2401daaeed1403c483a249c85559d1f0
Minkyu0424 <yng1404@gmail.com>
2024-07-29 01:23:32 +0900

    :sparkles: [공통]: sidenav map 함수로 렌더링

--- src/app/constants/common.ts
+++ src/app/constants/common.ts
@@ -1,3 +1,11 @@
+import {
+  OrderIcon,
+  PastOrderIcon,
+  ProductsIcon,
+  QuotationIcon,
+  StarIcon,
+} from '@/app/ui/iconPath';
+
 export const HEADER_TEXT = [
   '상품목록',
   '상품주문',
@@ -20,3 +28,27 @@ export const FOOTER_TEXT = [
 ];
 
 export const CONTRIBUTORS = ['YPYP', 'KimGorok', 'Minkyu'];
+
+export const SIDENAV_TEXT = [
+  '즐겨찾기',
+  '주문생성',
+  '견적서',
+  '주문내역',
+  '상품조회',
+];
+
+export const SIDENAV_ICONS = [
+  StarIcon,
+  OrderIcon,
+  QuotationIcon,
+  PastOrderIcon,
+  ProductsIcon,
+];
+
+export const SIDENAV_PATH = [
+  '/',
+  '/quotation',
+  '/quotation',
+  '/quotation',
+  '/quotation',
+];
