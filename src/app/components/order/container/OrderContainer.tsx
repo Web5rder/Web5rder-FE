2d9a6f2c827cf6ea4e623e31a726327c3b963524
kimgorok <kjyse2@naver.com>
2024-07-31 21:32:27 +0900

    🎨[견적서페이지]: 즐겨찾기 테두리 디자인 수정

--- src/app/components/order/container/OrderContainer.tsx
+++ src/app/components/order/container/OrderContainer.tsx
@@ -128,12 +128,12 @@ export default function OrderContainer() {
             {ORDER_TEXT[0]}
           </button>
           {state.showBookmark && (
-            <div className="absolute flex flex-col w-auto bg-white border-t-[1px] border-2 border-gray-2">
+            <div className="absolute flex flex-col w-auto bg-white">
               {pastOrder.map((order) => (
                 <button
                   key={order.past_order_id}
                   type="button"
-                  className="px-4 py-1 border-b border-gray-2 cursor-pointer last:border-none"
+                  className="px-4 py-1 border-b border-gray-2 cursor-pointer  border-t-[1px] border-2 "
                   onClick={() => setPastOrderId(order.past_order_id.toString())}
                 >
                   {order.name}
