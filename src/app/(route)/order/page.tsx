02399f3871e6056ae1884fc2f9c90a3047d37715
kimgorok <kjyse2@naver.com>
2024-08-01 02:57:14 +0900

    ✨[견적서_주문]: SideNavBar 적용 및 모달 css 수정

--- src/app/(route)/order/page.tsx
+++ src/app/(route)/order/page.tsx
@@ -1,5 +1,5 @@
+import SideNavBar from '@/app/components/common/layout/SideNavBar';
 import OrderContainer from '@/app/components/order/container/OrderContainer';
-import NavBar from '@/app/components/order/NavBar';
 import TopBar from '@/app/components/order/TopBar';
 
 export default function Order() {
@@ -7,7 +7,7 @@ export default function Order() {
     <section>
       <div className="bg-white w-full h-screen flex relative">
         {/* 네비게이션 바 */}
-        <NavBar />
+        <SideNavBar />
 
         {/* 탑바 */}
         <TopBar />
--- src/app/components/order/quotation/QuotationTable.tsx
+++ src/app/components/order/quotation/QuotationTable.tsx
@@ -20,9 +20,11 @@ export default function QuotationTable({ quotationInfo }: QuotationTableProps) {
       <div className="flex gap-x-3 text-xl font-semibold pb-1 pr-4 mt-[36px] whitespace-nowrap border-black border-b-[6px]">
         <div className="w-[7%] text-center">번호</div>
         <div className="w-[7%] text-center">{MODAL_TEXT[6]}</div>
-        <div className="w-[72%] text-center">{MODAL_TEXT[1]}</div>
+        <div className="w-[66%] text-center text-ellipsis overflow-hidden">
+          {MODAL_TEXT[1]}
+        </div>
         <div className="w-[7%] text-center">{MODAL_TEXT[2]}</div>
-        <div className="w-[7%] text-center">{MODAL_TEXT[5]}</div>
+        <div className="w-[13%] text-center">{MODAL_TEXT[5]}</div>
       </div>
 
       <div className="flex w-full h-[312px] flex-col gap-y-2 mt-2 overflow-y-scroll overflow-x-hidden border-b-[1px]">
@@ -34,9 +36,13 @@ export default function QuotationTable({ quotationInfo }: QuotationTableProps) {
             >
               <div className="w-[7%] text-center">{index + 1}</div>
               <div className="w-[7%] text-center">{itemData.category}</div>
-              <div className="w-[72%] text-center">{itemData.name}</div>
+              <div className="w-[66%] text-center text-ellipsis overflow-hidden">
+                {itemData.name}
+              </div>
               <div className="w-[7%] text-center">{itemData.count}</div>
-              <div className="w-[7%] text-center">{itemData.unit}</div>
+              <div className="w-[13%] text-center text-ellipsis overflow-hidden">
+                {itemData.unit}
+              </div>
             </div>
           );
         })}
