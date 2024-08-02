19fda9f38405f76578dd2e04cfe947278e244b32
김현중 <kjyse2@naver.com>
2024-08-02 23:48:44 +0900

    ✨[전체]: Conflict 오류 해결

--- src/app/(route)/order/page.tsx
+++ src/app/(route)/order/page.tsx
@@ -1,13 +1,14 @@
 import SideNavBar from '@/app/components/common/layout/SideNavBar';
 import OrderContainer from '@/app/components/order/container/OrderContainer';
 import TopBar from '@/app/components/order/TopBar';
+import { SIDENAV_TEXT } from '@/app/constants/common';
 
 export default function Order() {
   return (
     <section>
       <div className="bg-white w-full h-screen flex relative">
         {/* 네비게이션 바 */}
-        <SideNavBar />
+        <SideNavBar selected={SIDENAV_TEXT[1]} />
 
         {/* 탑바 */}
         <TopBar />
