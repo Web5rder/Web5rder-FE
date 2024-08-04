3391a19d969a9a54d079f59a36e9cdb5540de6dc
김현중 <kjyse2@naver.com>
2024-08-04 18:16:23 +0900

    ✨[견적서_주문]: getRequest 적용

--- src/app/service/getRequest.ts
+++ src/app/service/getRequest.ts
@@ -2,36 +2,23 @@ const SERVER_URL = process.env.NEXT_PUBLIC_SERVER;
 const commonHeaders = {
   'Content-Type': 'application/json',
 };
-export interface searchProductsProps {
-  namePrefix: string;
-  limit: string;
-  cachedTime: string;
-  token: string;
-}
+
+const getRequest = async (url: string, token?: string) => {
+  try {
+    const headers = token
+      ? { ...commonHeaders, 'access-token': token }
+      : { ...commonHeaders };
+    const response = await fetch(url, { headers }).then((res) => res.json());
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
 
 // 내 정보 조회
 export const getUsers = async (token: string) => {
-  try {
-    const url = `${SERVER_URL}/api/v1/users/me`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'access-token': token,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `내 정보 조회(getRequest) fetch 실패: ${response.statusText}`,
-      );
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error', error);
-    throw error;
-  }
+  const url = `${SERVER_URL}/api/v1/users/me`;
+  return getRequest(url, token);
 };
 
 // 상품 검색
@@ -41,78 +28,20 @@ export const getSearchProducts = async ({
   cachedTime,
   token,
 }: searchProductsProps) => {
-  try {
-    const url = `${SERVER_URL}/api/v1/products/search/recent?name_prefix=${namePrefix}&limit=${limit}&cached_time=${cachedTime}`;
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'access-token': token,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`getRequest 데이터 fetch 실패: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log(`Response data: ${JSON.stringify(data)}`);
-    return data;
-  } catch (error) {
-    console.error('Error: ', error);
-    throw error;
-  }
+  const url = `${SERVER_URL}/api/v1/products/search/recent?name_prefix=${namePrefix}&limit=${limit}&cached_time=${cachedTime}`;
+  return getRequest(url, token);
 };
 
 // 주문 내역 상세 불러오기
 export const getPastOrder = async (past_order_id: string) => {
-  try {
-    const url = `${SERVER_URL}/api/v1/past-order/${past_order_id}`;
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`getRequest 데이터 fetch 실패: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log(`Response data: ${JSON.stringify(data)}`);
-    return data;
-  } catch (error) {
-    console.error('Error', error);
-    throw error;
-  }
+  const url = `${SERVER_URL}/api/v1/past-order/${past_order_id}`;
+  return getRequest(url);
 };
 
 // 합계 금액 업데이트
 export const getQuotationTotal = async (quotation_id: string) => {
-  try {
-    const url = `${SERVER_URL}/api/v1/quotations/${quotation_id}/total`;
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`getRequest 데이터 fetch 실패: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log(`Response data: ${JSON.stringify(data)}`);
-    return data;
-  } catch (error) {
-    console.error('Error', error);
-    throw error;
-  }
+  const url = `${SERVER_URL}/api/v1/quotations/${quotation_id}/total`;
+  return getRequest(url);
 };
 
 // 거래처 주문 내역 조회
@@ -142,18 +71,6 @@ export const getClientPastOrder = async (client_id: string) => {
   } catch (error) {
     console.error('Error: ', error);
     throw error;
-  }
-};
-
-const getRequest = async (url: string, token?: string) => {
-  try {
-    const headers = token
-      ? { ...commonHeaders, 'access-token': token }
-      : { ...commonHeaders };
-    const response = await fetch(url, { headers }).then((res) => res.json());
-    return response;
-  } catch (error) {
-    return error;
   }
 };
 
