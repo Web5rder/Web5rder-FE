b37b5c673f890178e2b7a2aa28e8a558de6c96f2
김현중 <kjyse2@naver.com>
2024-08-06 23:12:21 +0900

    ✨[관리자]: 관리자 페이지 세팅

---
+++ src/app/(route)/admin/page.tsx
@@ -0,0 +1,5 @@
+import AdminContainer from '@/app/components/admin/container/AdminContainer';
+
+export default function Admin() {
+  return <AdminContainer />;
+}
---
+++ src/app/components/admin/container/AdminContainer.tsx
@@ -0,0 +1,7 @@
+export default function AdminContainer() {
+  return (
+    <section>
+      <div>관리자 페이지</div>
+    </section>
+  );
+}
