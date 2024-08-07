c209a4d08c62ac65f8352f38167693e4ee4ad7f0
김현중 <kjyse2@naver.com>
2024-08-07 17:44:34 +0900

    ✨[관리자]: 거래처 patchRequest 작성

--- src/app/api/admin/clients/[client_id]/comment/route.ts
+++ src/app/api/admin/clients/[client_id]/comment/route.ts
@@ -1,1 +1,24 @@
 // 거래처 특이사항 작성
+
+import { NextResponse } from 'next/server';
+import { patchAdminClientComment } from '@/app/service/patchRequest';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { client_id: string } },
+): Promise<NextResponse> {
+  try {
+    const { client_id } = params;
+    const { searchParams } = new URL(req.url);
+    const input_comment = searchParams.get('input_comment') || '';
+
+    const data = await patchAdminClientComment(client_id, input_comment);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('API Error:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
--- src/app/api/admin/clients/[client_id]/region/route.ts
+++ src/app/api/admin/clients/[client_id]/region/route.ts
@@ -1,1 +1,24 @@
 // 거래처 지역 선택
+
+import { NextResponse } from 'next/server';
+import { patchAdminClientRegion } from '@/app/service/patchRequest';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { client_id: string } },
+): Promise<NextResponse> {
+  try {
+    const { client_id } = params;
+    const { searchParams } = new URL(req.url);
+    const region = searchParams.get('region') || '';
+
+    const data = await patchAdminClientRegion(client_id, region);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('API Error:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
