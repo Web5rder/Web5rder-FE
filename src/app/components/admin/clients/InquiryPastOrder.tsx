22ba2b347769fe9e9dfcd978f605766eb6328e0e
김현중 <kjyse2@naver.com>
2024-08-09 01:01:23 +0900

    ✨[관리자]: 거래처 주문 내역 조회 UI 수정

--- src/app/components/admin/clients/InquiryPastOrder.tsx
+++ src/app/components/admin/clients/InquiryPastOrder.tsx
@@ -19,7 +19,7 @@ export default function InquiryPastOrder({ clientId }: ClientIdProps) {
 
   const renderTable = () => {
     return (
-      <table className="table-auto border-collapse border border-gray-3">
+      <table className=" border-collapse border border-gray-3 w-fit">
         <thead>
           <tr>
             <th className="admin-table-th">번호</th>
@@ -39,9 +39,9 @@ export default function InquiryPastOrder({ clientId }: ClientIdProps) {
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 border-2 p-8">
       <Button
-        className="border-2"
+        className="border-2 w-fit px-8"
         buttonText="실행"
         type="default"
         onClickHandler={handleGetPastOrders}
